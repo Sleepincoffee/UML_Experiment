a9e44399e9f45f81f7f0d2e8ecb096fdd995f197
Sleepincoffee <cs781503542@qq.com>
2016-05-13 11:08:03 +0800

    汇编程序环境

---
+++ masm611611/arg1.asm
@@ -0,0 +1,15 @@
+.model small
+.stack
+.data
+X dw 5
+Y dw 6
+Z dw 7
+W dw ?
+.code
+.startup
+mov ax,X
+add ax,Y
+add ax,Z
+mov W,ax
+.exit 0
+end
---
+++ masm611611/arg2.asm
@@ -0,0 +1,25 @@
+.model small
+.stack
+.data
+qvar dq 1234567887654321
+.code
+.startup
+mov al,byte ptr qvar[6]
+mov byte ptr qvar[7],al
+mov al,byte ptr qvar[5]
+mov byte ptr qvar[6],al
+mov al,byte ptr qvar[4]
+mov byte ptr qvar[5],al
+mov al,byte ptr qvar[3]
+mov byte ptr qvar[4],al
+mov al,byte ptr qvar[2]
+mov byte ptr qvar[3],al
+mov al,byte ptr qvar[1]
+mov byte ptr qvar[2],al
+mov al,byte ptr qvar[0]
+mov byte ptr qvar[1],al
+
+mov byte ptr qvar[0],0
+
+.exit 0
+end
